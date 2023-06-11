d6cf9e2f6eca62c9c93ff31cb0cf65f106b64080
Milton Figueredo Miles <gratno@gmail.com>
2023-06-10 21:41:32 -0500

    Add data apliccation yml

--- src/main/java/com/crudjava/mycrud/model/Book.java
+++ src/main/java/com/crudjava/mycrud/model/Book.java
@@ -1,4 +1,14 @@
 package com.crudjava.mycrud.model;
 
 public class Book {
+
+    public final long id;
+
+    public final String name;
+
+
+    public Book(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
---
+++ src/main/java/com/crudjava/mycrud/repositories/BookRespository.java
@@ -0,0 +1,10 @@
+package com.crudjava.mycrud.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRespository {
+
+
+
+}
